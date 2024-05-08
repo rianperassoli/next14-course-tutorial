4ca03ab3cd0a9238cf5fcc66a056c842a7d903eb
Rian Perassoli <rian.perassoli@gmail.com>
2024-05-08 17:25:57 -0300

    DB: creating a fixed single CRUD with prisma

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,7 +1,23 @@
-const PrismaExamle = () => {
+import prisma from "@/utils/db";
+
+const prismaHandlers = async () => {
+  await prisma.task.create({ data: { content: "wake up" } });
+
+  const allTasks = await prisma.task.findMany({
+    orderBy: { createdAt: "desc" },
+  });
+
+  return allTasks;
+};
+
+const PrismaExamle = async () => {
+  const tasks = await prismaHandlers();
   return (
     <div>
       <h1 className="text-7xl">PrismaExamle Page</h1>
+      {tasks.map((task) => (
+        <h2 className="text-xl py-2">{task.content}</h2>
+      ))}
     </div>
   );
 };
